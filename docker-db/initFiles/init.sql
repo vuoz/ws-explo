6bc6d7b9c6d235f8eb84762dd0c8b12745026807
zvup <121556153+zvup@users.noreply.github.com>
2024-07-09 20:27:14 +0200

    updated pg init files according to schema changes

--- docker-db/initFiles/init.sql
+++ docker-db/initFiles/init.sql
@@ -2,5 +2,23 @@ CREATE table usertable(
    username text,
    password text,
    key text,
-   auth text
+   userid text NOT NULL,
+   CONSTRAINT usertable_pkey PRIMARY KEY (userid),
+   CONSTRAINT usertable_username UNIQUE (username)
 );
+
+CREATE INDEX IF NOT EXISTS "usertable_userId_idx"
+    ON usertable USING btree
+    (userid COLLATE pg_catalog."default" ASC NULLS LAST)
+    WITH (deduplicate_items=True)
+    TABLESPACE pg_default;
+
+
+
+Create table session(
+   userid  text NOT NULL,
+   time bigint NOT NULL,
+   token text NOT NULL,
+   CONSTRAINT session_pkey PRIMARY KEY (userid),
+   CONSTRAINT session_token UNIQUE  (token)
+)
